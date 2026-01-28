cbfc9a1d41bf336a4394ca1ea461d010fdb38e76
taisei-yukawa <m3245100500@std.kitami-it.ac.jp>
2026-01-28 19:07:43 +0900

    Change track_id classical 1027928

--- app.py
+++ app.py
@@ -91,7 +91,7 @@ PRIMARY_LIMIT: int = 10
 POP_LIMIT: int = 10
 
 QUERY_TRACKS: Dict[str, str] = {
-    "classical": "1027927",
+    "classical": "1027928",
     "jazz": "1069786",
     "rock": "1134644",
     "hiphop": "1157595",
